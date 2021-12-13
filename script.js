260164897bb43e1159534689d4635aa8157b9ec5
DexREAU <andromedog2017@gmail.com>
2021-12-13 12:55:22 -0300

    v9

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
 ScrollReveal().reveal('body, h1, .underdog_sec article h2, .underdog_sec article h3, .why_sec span, .why_sec p, .web3_sec p, .how_sec p, .products_sec h6, .products_sec p, .products_list li, .products_list img, .blog_section article', {
-    duration:1600,
-    delay:100,
+    duration:1200,
     easing: 'ease',
 });
-    
-// let windowPosition = window.pageYOffset;
 
-// document.body.onscroll = function() {
-//     windowPosition = window.pageYOffset;
-// }
+const timelineSec = document.querySelector('.timeline_sec')
+
+
+ScrollReveal().reveal(timelineSec, {
+    duration:1200,
+    easing: 'ease',
+    afterReveal: function (el) {
+        
+    }
+});
